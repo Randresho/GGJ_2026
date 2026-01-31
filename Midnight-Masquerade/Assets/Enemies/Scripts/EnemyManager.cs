b9dba7cc1314d95d512bd1974d0dfa7c045b67e6
Andres Hernandez <randresho@gmail.com>
2026-01-31 10:17:54 -0600

    WIP Enemy

---
+++ Midnight-Masquerade/Assets/Enemies/Scripts/EnemyManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    private enum EnemyType { Tank, Rouge, CrowdControl, }
+    [SerializeField] EnemyType enemyType;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
